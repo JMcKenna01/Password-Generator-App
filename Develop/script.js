e83ec2ffa42f9152517c14cda8bd6ca097c5bd94
jmckenna01 <jmckenna2026@gmail.com>
2023-11-06 21:52:31 -0500

    updated script.js

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,48 @@ function writePassword() {
 
 }
 
+// Function to generate a random password based on user criteria
+function generatePassword() {
+  var passwordLength = prompt("Enter the desired password length (between 8 and 128 characters):");
+
+  // Validate the password length
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Please enter a valid password length between 8 and 128.");
+    return "";
+  }
+
+  var useLowercase = confirm("Include lowercase letters?");
+  var useUppercase = confirm("Include uppercase letters?");
+  var useNumeric = confirm("Include numbers?");
+  var useSpecial = confirm("Include special characters?");
+
+  // Define character sets based on user criteria
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+  var allChars = "";
+
+  if (useLowercase) allChars += lowercaseChars;
+  if (useUppercase) allChars += uppercaseChars;
+  if (useNumeric) allChars += numericChars;
+  if (useSpecial) allChars += specialChars;
+
+  if (allChars === "") {
+    alert("At least one character type must be selected.");
+    return "";
+  }
+
+  var password = "";
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars.charAt(randomIndex);
+  }
+
+  return password;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
